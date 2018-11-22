80629fb15d058c81fd44eedcaeb2e1928d17f4f6
DAB0mB <emanor6@gmail.com>
2018-11-22 12:16:08 +0800

    Add babel-preset-stage-X

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {
 } from '@babel/core'
 import { resolve, extname } from 'path'
 import fs from './libs/fs'
-import plugin from './plugin'
+import pluckPlugin from './babel-plugin'
 import { merge } from './utils'
 
 const gqlExtensions = [
@@ -104,8 +104,36 @@ gqlPluckFromCodeString.sync = (codeString, customConfig = {}) => {
 
 const createConfig = (...customConfigs) => {
   return merge({
-    plugins: [plugin],
-    presets: ['@babel/typescript'],
+    plugins: [
+      pluckPlugin,
+
+      // Stage 0
+      '@babel/plugin-proposal-function-bind',
+
+      // Stage 1
+      '@babel/plugin-proposal-export-default-from',
+      '@babel/plugin-proposal-logical-assignment-operators',
+      ['@babel/plugin-proposal-optional-chaining', { 'loose': true }],
+      ['@babel/plugin-proposal-pipeline-operator', { 'proposal': 'minimal' }],
+      ['@babel/plugin-proposal-nullish-coalescing-operator', { 'loose': true }],
+      '@babel/plugin-proposal-do-expressions',
+
+      // Stage 2
+      ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+      '@babel/plugin-proposal-function-sent',
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-numeric-separator',
+      '@babel/plugin-proposal-throw-expressions',
+
+      // Stage 3
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-syntax-import-meta',
+      ['@babel/plugin-proposal-class-properties', { 'loose': true }],
+      '@babel/plugin-proposal-json-strings',
+    ],
+    presets: [
+      '@babel/typescript',
+    ],
     code: false,
     ast: false,
   }, ...customConfigs)
